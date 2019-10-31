a89b8a3a45269a86a75d7f03d778e48c89c473a8
sanga05 <spamme017@yandex.com>
2019-10-31 17:16:15 +0000

    DjangoUpdated

---
+++ dRestService/views.py
@@ -0,0 +1,9 @@
+
+from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework import status
+from rest_framework.response import Response
+# class getUserdet(RetrieveUpdateDestroyAPIView):
+from rest_framework.decorators import api_view
+@api_view(['GET', 'POST', ])
+def getUser(request):
+         return Response("everthyng ok", status=status.HTTP_200_OK,template_name='articles.html')
