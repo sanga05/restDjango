1a2b1c3207b0f3328a7ab56ac33c88a4f3f3c76f
sanga05 <spamme017@yandex.com>
2019-10-31 17:44:52 +0000

    changes

--- dRestService/urls.py
+++ dRestService/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import getUser
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^users/',getUser)
+    url(r'^users/',views.getUser)
 ]
